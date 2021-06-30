c538ca7b941d1bb1762bdf21b9797bc88e1e10fd
Patrykoss <adresikps@gmail.com>
2021-06-30 18:46:33 +0200

    Added delete behaviour for answers tree

--- Server/Migrations/20210630164241_CascadeDeleteAnswers.Designer.cs
+++ Server/Migrations/20210630164241_CascadeDeleteAnswers.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Quanda.Server.Data;
 
 namespace Quanda.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210630164241_CascadeDeleteAnswers")]
+    partial class CascadeDeleteAnswers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
