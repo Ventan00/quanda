d96c743988c447f5f78faf568562f816205bef93
xlegodroit <xxvege@gmail.com>
2021-06-30 04:05:23 +0200

    Added validation for LoginDTO

--- Shared/DTOs/Requests/LoginDTO.cs
+++ Shared/DTOs/Requests/LoginDTO.cs
@@ -1,8 +1,15 @@
-﻿namespace Quanda.Shared.DTOs.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Quanda.Shared.DTOs.Requests
 {
     public class LoginDTO
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
         public string RawPassword { get; set; }
     }
 }
