521cf4889e9225358b84ec57455b2135db8447a8
xlegodroit <xxvege@gmail.com>
2021-07-05 19:31:34 +0200

    Modified SmtpService

--- Server/Services/Implementations/SmtpService.cs
+++ Server/Services/Implementations/SmtpService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Quanda.Server.Services.Interfaces;
 
@@ -9,22 +10,29 @@ namespace Quanda.Server.Services.Implementations
     public class SmtpService : ISmtpService
     {
         private readonly IConfigurationSection _smtpConfigSection;
-        public SmtpService(IConfiguration configuration)
+        private readonly string _hostBaseUrl;
+
+        public SmtpService(IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
         {
             _smtpConfigSection = configuration.GetSection("SmtpSettings");
+
+            if (httpContextAccessor.HttpContext is not null)
+            {
+                _hostBaseUrl = $"https://{httpContextAccessor.HttpContext.Request.Host}";
+            }
         }
 
 
         public async Task SendRegisterConfirmationEmailAsync(string email, string confirmationCode)
         {
-            var smtpClient = CreateSmtpClient();
-            await smtpClient.SendMailAsync(_smtpConfigSection["Email"], email, "QUANDA - Complete register", $"https://localhost:44358/api/accounts/confirm-email/{confirmationCode}");
+            var href = $"{_hostBaseUrl}/api/accounts/confirm-email/{confirmationCode}";
+            await SendEmailAsync(email, "QUANDA - Complete register", $"{href}");
         }
 
         public async Task SendPasswordRecoveryEmailAsync(string email, string recoveryJwt, int idUser)
         {
-            var smtpClient = CreateSmtpClient();
-            await smtpClient.SendMailAsync(_smtpConfigSection["Email"], email, "QUANDA - Recover password", $"https://localhost:44358/recover/password/reset?uuid={idUser}&recovery_token={recoveryJwt}");
+            var href = $"{_hostBaseUrl}/recover/password/reset?uuid={idUser}&recovery_token={recoveryJwt}";
+            await SendEmailAsync(email, "QUANDA - Recover password", $"{href}");
         }
 
         private SmtpClient CreateSmtpClient()
@@ -35,6 +43,12 @@ namespace Quanda.Server.Services.Implementations
                 Credentials = new NetworkCredential(_smtpConfigSection["Email"], _smtpConfigSection["Password"]),
                 EnableSsl = bool.Parse(_smtpConfigSection["EnableSsl"]),
             };
+        }
+
+        private async Task SendEmailAsync(string recipientEmail, string subject, string body)
+        {
+            var smtpClient = CreateSmtpClient();
+            await smtpClient.SendMailAsync(_smtpConfigSection["Email"], recipientEmail, subject, $"{body}");
         }
     }
 }
