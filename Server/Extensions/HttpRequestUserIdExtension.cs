f388ce0c5fc7ebcad18369939267d46e11a84692
xlegodroit <xxvege@gmail.com>
2021-07-08 01:47:02 +0200

    Added Http request user id extension

---
+++ Server/Extensions/HttpRequestUserIdExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Quanda.Server.Extensions
+{
+    public static class HttpRequestUserIdExtension
+    {
+        public static int? IdUser;
+        public static void SetUserId(this HttpRequest httpRequest, ClaimsPrincipal claimsPrincipal)
+        {
+            var nameIdentifier = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            IdUser = int.Parse(nameIdentifier);
+        }
+
+        public static int? GetUserId(this HttpRequest httpRequest)
+        {
+            return IdUser;
+        }
+    }
+}
