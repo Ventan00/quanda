6186a68346fe99e0ecd343e6e22b4081c920478c
xlegodroit <xxvege@gmail.com>
2021-08-11 22:06:12 +0200

    Added claimsPrincipal extension for getting logged user id

---
+++ Server/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace Quanda.Server.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static int? GetId(this ClaimsPrincipal claimsPrincipal)
+        {
+            var idUser = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+            var canParse = int.TryParse(idUser, out var parsedIdUser);
+            if (canParse)
+                return parsedIdUser;
+            return null;
+        }
+    }
+}
